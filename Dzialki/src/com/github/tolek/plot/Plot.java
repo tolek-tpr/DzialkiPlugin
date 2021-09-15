3c10dd5388ff005bc68ce86293845712dce03051
Michał Niedźwiedzki <michal@manse.fr>
2021-09-15 10:47:48 +0200

    Refactored loading/saving, fixed allowed users list

--- Dzialki/src/com/github/tolek/plot/Plot.java
+++ Dzialki/src/com/github/tolek/plot/Plot.java
@@ -7,95 +7,123 @@ import org.bukkit.entity.Player;
 
 public class Plot {
 
-	public static int MAX_SIZE = 100;
-	public static int MAX_PLOTS = 2;
+    public static int MAX_SIZE = 100;
+    public static int MAX_PLOTS = 2;
 
-	public int x;
-	public int z;
-	public int sizeX;
-	public int sizeZ;
+    public int x;
+    public int z;
+    public int sizeX;
+    public int sizeZ;
 
-	public String admin;
-	public String name;
-	public List<String> allowedUsers = new ArrayList<String>();
+    public String admin;
+    public String name;
+    public List<String> allowedUsers = new ArrayList<String>();
 
-	public Plot(int x, int z, int sizeX, int sizeZ, String admin, String name) throws Exception {
-		if (sizeX > MAX_SIZE || sizeZ > MAX_SIZE) {
-			throw new Exception("Plot Too Large");
-		}
-		this.x = x;
-		this.z = z;
-		this.sizeX = sizeX;
-		this.sizeZ = sizeZ;
-		this.admin = admin;
-		this.name = name;
-	}
-	
-	public Plot(int x, int z, int sizeX, int sizeZ, String admin, String name, String[] allowedUsers) throws Exception {
-		if (sizeX > MAX_SIZE || sizeZ > MAX_SIZE) {
-			throw new Exception("Plot Too Large");
-		}
-		this.x = x;
-		this.z = z;
-		this.sizeX = sizeX;
-		this.sizeZ = sizeZ;
-		this.admin = admin;
-		this.name = name;
-		this.allowedUsers = List.of(allowedUsers);
-	}
+    public Plot(int x, int z, int sizeX, int sizeZ, String admin, String name) throws Exception {
+        if (sizeX > MAX_SIZE || sizeZ > MAX_SIZE) {
+            throw new Exception("Plot Too Large");
+        }
+        this.x = x;
+        this.z = z;
+        this.sizeX = sizeX;
+        this.sizeZ = sizeZ;
+        this.admin = admin;
+        this.name = name;
+    }
 
-	public boolean overlaps(Plot p) {
-		int xs1 = this.x;
-		int xe1 = xs1 + this.sizeX;
-		int xs2 = p.x;
-		int xe2 = xs2 + p.sizeX;
+    public Plot(int x, int z, int sizeX, int sizeZ, String admin, String name, List<String> allowedUsers) throws Exception {
+        if (sizeX > MAX_SIZE || sizeZ > MAX_SIZE) {
+            throw new Exception("Plot Too Large");
+        }
+        this.x = x;
+        this.z = z;
+        this.sizeX = sizeX;
+        this.sizeZ = sizeZ;
+        this.admin = admin;
+        this.name = name;
+        this.allowedUsers = allowedUsers;
+    }
 
-		int zs1 = this.z;
-		int ze1 = zs1 + this.sizeZ;
-		int zs2 = p.z;
-		int ze2 = zs2 + p.sizeZ;
+    public boolean overlaps(Plot p) {
+        int xs1 = this.x;
+        int xe1 = xs1 + this.sizeX;
+        int xs2 = p.x;
+        int xe2 = xs2 + p.sizeX;
 
-		boolean overlapsX = xs2 >= xs1 && xs2 <= xe1 || xe2 >= xs1 & xe2 <= xe1;
-		boolean overlapsZ = zs2 >= zs1 && zs2 <= ze1 || ze2 >= zs1 & ze2 <= ze1;
+        int zs1 = this.z;
+        int ze1 = zs1 + this.sizeZ;
+        int zs2 = p.z;
+        int ze2 = zs2 + p.sizeZ;
 
-		return overlapsX || overlapsZ;
-	}
-	
-	public boolean overlaps(int x, int z) {
-		int xs1 = this.x;
-		int xe1 = xs1 + this.sizeX;
+        boolean overlapsX = xs2 >= xs1 && xs2 <= xe1 || xe2 >= xs1 & xe2 <= xe1;
+        boolean overlapsZ = zs2 >= zs1 && zs2 <= ze1 || ze2 >= zs1 & ze2 <= ze1;
 
-		int zs1 = this.z;
-		int ze1 = zs1 + this.sizeZ;
+        return overlapsX || overlapsZ;
+    }
 
-		boolean overlapsX = x >= xs1 && x <= xe1;
-		boolean overlapsZ = z >= zs1 && z <= ze1;
+    public boolean overlaps(int x, int z) {
+        int xs1 = this.x;
+        int xe1 = xs1 + this.sizeX;
 
-		return overlapsX && overlapsZ;
-		
-	}
+        int zs1 = this.z;
+        int ze1 = zs1 + this.sizeZ;
 
-	public String toString() {
-		return name + " [" + x + ":" + z + "][" + sizeX + ":" + sizeZ + "] by " + admin;
-	}
+        boolean overlapsX = x >= xs1 && x <= xe1;
+        boolean overlapsZ = z >= zs1 && z <= ze1;
 
-	public boolean isOwnedBy(Player p) {
-		return admin.equals(p.getUniqueId().toString());
-	}
+        return overlapsX && overlapsZ;
 
-	public boolean isOwnedBy(String id) {
-		return admin.equals(id);
-	}
+    }
 
-	public boolean addUser(String id) {
-		return "".equals(id) && allowedUsers.add(id);
-	}
+    public boolean isOwnedBy(Player p) {
+        return admin.equals(p.getUniqueId().toString());
+    }
 
-	public boolean isAccessibleBy(Player p) {
-		return p != null && allowedUsers.contains(p.getUniqueId().toString());
-	}
-	
-	public boolean removeUser(String id) {
-		return allowedUsers.remove(id);
-	}
+    public boolean isOwnedBy(String id) {
+        return admin.equals(id);
+    }
+
+    public boolean isAccessibleBy(Player p) {
+        return p != null && allowedUsers.contains(p.getUniqueId().toString());
+    }
+
+    public boolean isAccessibleBy(String id) {
+        return id != null && allowedUsers.contains(id);
+    }
+
+    public boolean addUser(Player p) {
+        return p != null && allowedUsers.add(p.getUniqueId().toString());
+    }
+
+    public boolean addUser(String id) {
+        return "".equals(id) && allowedUsers.add(id);
+    }
+
+    public boolean removeUser(Player p) {
+        return allowedUsers.remove(p.getUniqueId().toString());
+    }
+
+    public boolean removeUser(String id) {
+        return allowedUsers.remove(id);
+    }
+
+    public static Plot importFromStorage(String line) throws Exception {
+        String[] items = line.split("\\|");
+        return new Plot(Integer.parseInt(items[0]),
+                Integer.parseInt(items[1]),
+                Integer.parseInt(items[2]),
+                Integer.parseInt(items[3]),
+                items[4],
+                items[5],
+                List.of(items[6].split("\\,")));
+    }
+
+    public String exportToString() {
+        return String.join("|", name, String.valueOf(x), String.valueOf(z), String.valueOf(sizeX), String.valueOf(sizeZ), admin, String.join(",", allowedUsers));
+    }
+
+    public String toString() {
+        return name + " [" + x + ":" + z + "][" + sizeX + ":" + sizeZ + "] by " + admin;
+    }
+
 }
--- Dzialki/src/com/github/tolek/plot/PlotManager.java
+++ Dzialki/src/com/github/tolek/plot/PlotManager.java
@@ -7,11 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-
-import net.md_5.bungee.api.ChatColor;
+import java.util.stream.Collectors;
 
 public class PlotManager {
 
@@ -46,98 +42,53 @@ public class PlotManager {
 	}
 
 	public Plot getPlotbyName(String name) {
-		int count = 0;
 		for (Plot p : plots) {
-			if (name.equals(p.name)) {
+			if (name.equals(p.name))
 				return p;
-			}
 		}
 		return null;
 	}
 	
 	public Plot getPlotByLocation(int x, int z) {
-		String name = "";
 		for (Plot p : plots) {
-			if(p.overlaps(x, z)) {
-				name = p.name;
+			if (p.overlaps(x, z))
 				return p;
-			}
 		}
 		return null;
 	}
-	
-	public void addAllowedUser(String user, String plot) {
-		
-	}
 
-	public void save() { 
-		String data = "";
-		for (Plot p : plots) {
-			String allowedUsersList = "";
-			for (String userId : p.allowedUsers) {
-				allowedUsersList += userId + ',';
-			}
-			data += p.x + "|" + p.z + "|" + p.sizeX + "|" + p.sizeZ + "|" + p.admin + "|" + p.name + "|" + p.admin + "," + allowedUsersList + "\n";
-		}
-
-		FileOutputStream outputStream = null;
+	public void save() {
+		String data = plots.stream().map(Plot::exportToString).collect(Collectors.joining("\n"));
+		System.out.println("Saving file\n" + data);
 		try {
-			outputStream = new FileOutputStream(FILE_NAME, false);
-			outputStream.write(data.getBytes());
+			FileOutputStream writer = new FileOutputStream(FILE_NAME, false);
+			writer.write(data.getBytes());
+			writer.close();
 		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (outputStream != null) {
-				try {
-					outputStream.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+			System.err.println(("Could not write file"));
 		}
 	}
 
 	public void load() {
 		try {
-			Scanner myReader = new Scanner(new File(FILE_NAME));
-			while (myReader.hasNextLine()) {
-				String[] line = myReader.nextLine().split("\\|");
-				int x = Integer.parseInt(line[0]);
-				int z = Integer.parseInt(line[1]);
-				int sizeX = Integer.parseInt(line[2]);
-				int sizeZ = Integer.parseInt(line[3]);
-				String admin = line[4];
-				String name = line[5];
-				String[] allowedUsers = line[6].split("\\,");
-				// System.out.println("x: " + x + " z: " + z + " sx: " + sizeX + " sz: " + sizeZ
-				// + " admin: " + admin);
-
-				Plot plot;
+			Scanner reader = new Scanner(new File(FILE_NAME));
+			while (reader.hasNextLine()) {
 				try {
-					plot = new Plot(x, z, sizeX, sizeZ, admin, name, allowedUsers);
-					if (add(plot)) {
-						System.out.println("Added plot " + plot.toString());
-					} else {
+					Plot plot = Plot.importFromStorage(reader.nextLine());
+					if (!add(plot))
 						System.out.println("Could not add plot");
-					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-
 			}
-			myReader.close();
+			reader.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("File Not Found! (Don't worry it will be created as soon as someone makes a plot)");
-			e.printStackTrace();
+			System.out.println("Plots file not found, not loading");
 		}
 	}
 
 	public String toString() {
-		String out = "Plots: " + plots.size() + "\n";
-		for (Plot p : plots) {
-			out += p.toString() + "\n";
-		}
-		return out;
+		return "Plots: " + plots.size() + "\n" + plots.stream().map(Plot::toString).collect(Collectors.joining("\n"));
 	}
 
 }
