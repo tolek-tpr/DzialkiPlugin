d196ee4a1769c8241b4f559fce2f10e625e8fd3d
Michał Niedźwiedzki <michal@manse.fr>
2021-09-17 19:57:25 +0200

    Fixed structure of tests

---
+++ src/test/java/com/github/tolek/dzialki/plot/PlotTest.java
@@ -0,0 +1,48 @@
+package com.github.tolek.dzialki.plot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlotTest {
+
+    @Test
+    void testOverlaps() {
+        try {
+            Plot plot = new Plot("test1", 0, 0, 10, 10, "player1");
+            assertFalse(plot.overlaps(-1, -1), "Should return false when outside of plot area to the left/top");
+            assertFalse(plot.overlaps(11, -1), "Should return false when outside of plot area to the right/top");
+            assertFalse(plot.overlaps(11, -1), "Should return false when outside of plot area to the left/bottom");
+            assertFalse(plot.overlaps(11, 11), "Should return false when outside of plot area to the right/bottom");
+            assertFalse(plot.overlaps(-1, 5), "Should return false when outside of plot area to the left");
+            assertFalse(plot.overlaps(11, 5), "Should return false when outside of plot area to the right");
+            assertFalse(plot.overlaps(5, -1), "Should return false when outside of plot area to the top");
+            assertFalse(plot.overlaps(5, 11), "Should return false when outside of plot area to the bottom");
+            assertTrue(plot.overlaps(5, 5), "Should return true when inside of plot area");
+        } catch (Exception e) {
+            fail("Should not throw at plot creation");
+        }
+    }
+
+    @Test
+    void testIsOwnedBy() {
+        try {
+            Plot plot = new Plot("test1", 0, 0, 10, 10, "owner");
+            assertTrue(plot.isOwnedBy("owner"), "Should return true when owned by owner");
+            assertFalse(plot.isOwnedBy("some-other-player"), "Should return false when owned by someone else");
+        } catch (Exception e) {
+            fail("Should not throw at plot creation");
+        }
+    }
+
+    @Test
+    void testExportToStorage() {
+        try {
+            Plot plot = new Plot("test1", 0, 0, 10, 10 ,"owner", List.of("user1", "user2"));
+            String actual = plot.exportToStorage();
+            String expected = "test1|0|0|10|10|owner|user1,user2";
+            assertEquals("Should format the plot for export", expected, actual);
+        }
+    }
+
+}
